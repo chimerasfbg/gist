28ad94cda9383e21dde40d4c2ea1fca608d9f288
chimerasfbg <chimerasf.bg@gmail.com>
2022-06-29 23:07:17 +0200

    dot

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Posts from "./components/posts";
-import Pagination from "./components/pagination";
-// import { Octokit } from "@octokit/rest";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(30);
+  const [postPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPost = async () => {
       setLoading(true);
-      const res = await axios.get("https://api.github.com/gists/public");
+      const res = await axios.get(
+        "https://api.github.com/gists/public?page=2?per_page=100?since=YYYY-MM-DDTHH:MM:SSZ"
+      );
 
       setPosts(res.data);
       setLoading(false);
@@ -23,20 +24,18 @@ function App() {
     fetchPost();
   }, []);
 
-  // .map((post) => console.log(post)
-
-  console.log(posts.map((post) => console.log(post.files)));
-
   const indexOfLastPost = currentPage * postPerPage;
   const indexOfFirstPost = indexOfLastPost - postPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="app">
       <h1 className="text-primary mb-3">Task Data</h1>
       <Posts posts={currentPosts} loading={loading} />
       <Pagination
+        loading={setLoading}
         postPerPage={postPerPage}
         paginate={paginate}
         totalPosts={posts.length}
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,20 +1,42 @@
-import React, { Children } from "react";
+import React from "react";
 
-const posts = ({ posts, loading }) => {
+const Posts = ({ posts, loading }) => {
+  const boxClick = (e) => {
+    e.stopPropagation();
+    if (e.target.classList.contains("bb")) {
+      e.target.classList.toggle("change");
+    }
+  };
+
   if (loading) {
-    return <h2>Loading...</h2>;
+    return (
+      <h2 d-flex justify-content-start align-items-center>
+        Loading...
+      </h2>
+    );
   }
 
   return (
-    <ul className="list-group mb-4">
+    <ul className="list-group mb-4 ">
       {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          <img src={post.owner.avatar_url} alt="img" />
-          <p>{post.id}</p>
+        <li
+          key={post.id}
+          className="list-group-item d-flex flex-row justify-content-start align-items-center bb "
+          onClick={boxClick}
+        >
+          <img
+            src={post.owner.avatar_url}
+            alt="img"
+            className="img-thumbnail img bb"
+          />
+
+          {Object.keys(post.files).map(function (keyName, keyIndex) {
+            return <p className="ms-4">{post.files[keyName].filename}</p>;
+          })}
         </li>
       ))}
     </ul>
   );
 };
 
-export default posts;
+export default Posts;
